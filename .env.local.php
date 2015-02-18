890ac5a3340cb817d34f74bed7fa51d6dd77342a
Jeff Berry <jeff@portonefive.com>
2015-02-18 10:19:12 -0700

    Test

--- .env.local.php
+++ .env.local.php
@@ -5,9 +5,9 @@
 /*----------------------------------------------------*/
 return array(
 
-    'DB_NAME'       => 'northforkchampionship',
-    'DB_USER'       => 'northforkchampio',
-    'DB_PASSWORD'   => 'E5g#68*!c".$s@fd?w2m',
+    'DB_NAME'       => 'themosis',
+    'DB_USER'       => 'themosis',
+    'DB_PASSWORD'   => 'themosis',
     'DB_HOST'       => 'localsql',
     'WP_HOME'       => 'http://themosis.local',
     'WP_SITEURL'    => 'http://themosis.local/cms'
